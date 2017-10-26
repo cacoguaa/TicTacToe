adb528e9db66e7049ea4543dd7e7dfff4921df22
AceMaster <ace7ace@hotmail.com>
2017-10-26 18:37:09 -0500

    Basic connection with firebase added

--- app/src/main/java/co/edu/unal/tictactoe/MultiplayerActivity.java
+++ app/src/main/java/co/edu/unal/tictactoe/MultiplayerActivity.java
@@ -3,9 +3,16 @@ package co.edu.unal.tictactoe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MultiplayerActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,11 @@ public class MultiplayerActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
 
+    }
+
+    public void send(View view) {
+        // Write a message to the database
+        EditText name  = (EditText)findViewById(R.id.name);
+        mDatabase.push().setValue(name.getText().toString());
     }
 }
